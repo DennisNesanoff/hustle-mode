301fc7249e83a6d3097401fcedce1a64d1601418
Dennis Nesanoff <unknownorganizer@calendar.google.com>
2019-01-30 22:42:03 +0300

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Dennis Nesanoff on 30/01/2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
